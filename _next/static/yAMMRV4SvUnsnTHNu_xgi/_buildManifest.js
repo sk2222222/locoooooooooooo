36d372876028830f003ba13b6560eefcaa35e1b4
sk2222222 <>
2023-09-21 18:18:28 +0000

    Updates

---
+++ _next/static/yAMMRV4SvUnsnTHNu_xgi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a29dd428a5143455.css","static/chunks/pages/index-65d10c7e25b97b12.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
